e72dd9bd8f99635d98cee281db351e1e67bd2d6a
iqsuz <mehmetaliipsuz@hotmail.com>
2020-07-27 22:01:57 +0100

    logic has been changed in parser function

--- NMEA/main.c
+++ NMEA/main.c
@@ -2,8 +2,8 @@
 
 int main(){
     char *str = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76";
-    StructGGA GGA_Struct;
-
-
+    double a;
+    a = str2double(str+6,str+9);
+    printf("%f",a);
     return 0;
 }
