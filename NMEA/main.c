eaff80aed36404116050167a82329f38d37811b0
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-07-27 22:49:20 -0500

    parse and str2double functions are updated.

--- NMEA/main.c
+++ NMEA/main.c
@@ -1,9 +1,11 @@
 #include "../NMEA.h"
 
 int main(){
-    char *str = "$GPGGA,092750.000,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76";
-    double a;
-    a = str2double(str+6,str+9);
-    printf("%f",a);
+    char *str = "$GPGGA,092750.123,5321.6802,N,00630.3372,W,1,8,1.03,61.7,M,55.2,M,,*76";
+    StructGGA GGA_Struct;
+
+    parse_gga(str, &GGA_Struct);
+    printf("%f\n",GGA_Struct.time);
+    printf("%f\n",GGA_Struct.lat);
     return 0;
 }
